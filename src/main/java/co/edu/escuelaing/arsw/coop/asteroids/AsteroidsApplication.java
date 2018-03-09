d9cf2aeed7b4ba7e6f20fc793fb2e93da4b42e47
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2018-03-08 22:15:15 -0500

    Inicialización de Proyecto. 	- Agregadas dependencias Spring 	- Clase Main Spring 	- Agregado gitignore

---
+++ src/main/java/co/edu/escuelaing/arsw/coop/asteroids/AsteroidsApplication.java
@@ -0,0 +1,15 @@
+package co.edu.escuelaing.arsw.coop.asteroids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * //TODO
+ * @author Daniel Ospina
+ */
+@SpringBootApplication
+public class AsteroidsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsteroidsApplication.class, args);
+    }
+}
