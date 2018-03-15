e15a913ab4990e8df44e959452c554764508d011
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2018-03-15 14:40:54 -0500

    Arreglado estilo de codigo segun sugerencias de Codacy.com

--- src/main/resources/static/js/Game.js
+++ src/main/resources/static/js/Game.js
@@ -1,35 +1,19 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create
-    }
-};
-
-var game = new Phaser.Game(config);
+/* global Phaser */
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
+    this.load.setBaseURL("http://labs.phaser.io");
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, "sky");
 
-    var particles = this.add.particles('red');
+    var particles = this.add.particles("red");
 
     var emitter = particles.createEmitter({
         speed: 200,
@@ -46,4 +30,20 @@ function create ()
     emitter.startFollow(logo);
 }
 
-console.log("Testing");
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
