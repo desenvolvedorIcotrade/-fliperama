5b2a187d4100c8ac3b1c15fbd2396390fc8abbb3
Juan Felipe Ortiz <jfelipeon@outlook.com>
2018-04-05 08:58:55 -0500

    Correcion celula de combustion y HU8-CelulaVida 	-Se corrigieron los errores que presentaban la HU7 con respecto a los puntos nulos y sus velocidades siempre positivas 	-Una vida sale en el escenario y se queda estatica en su poscicion a menos que un jugador la coja, estan salen cada minuto con un maximo de tres vida por escenario

--- src/main/resources/static/js/asteroidsController.js
+++ src/main/resources/static/js/asteroidsController.js
@@ -1,10 +1,5 @@
 /* global Phaser, ClientModule */
 
-var player;
-var cursorKeys;
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, "canvasGame");
-var connected = false;
-
 var GameModule = (function () {
     
     var playerList = [];
@@ -33,6 +28,8 @@ var cursorKeys;
 var fullCells;
 var bg;
 var timerFull;
+var lives;
+var timerLives;
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, "canvasGame");
 
@@ -43,12 +40,14 @@ var statusMain = {
         game.load.image("background", "assets/fondoSpace1.png");
         game.load.spritesheet("player", "assets/shipP1.png", 26, 40);
         game.load.image("fullCell","assets/fullCell.png");
-
+        game.load.image("life","assets/life.png");
     },
     create: function () {
         bg = game.add.tileSprite(0, 0, 800, 600, "background");
+        
         var playerX = Math.floor(Math.random() * 801);
         var playerY = Math.floor(Math.random() * 601);
+        
         player = game.add.sprite(playerX, playerY, "player");
         player.anchor.setTo(0.5);
         player.animations.add("acceleration", [0, 1], 15, true);
@@ -57,21 +56,24 @@ var statusMain = {
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.enable(player);
+        
         player.body.collideWorldBounds = true;
         player.body.maxVelocity.x = 60;
         player.body.maxVelocity.y = 60;
         
         game.stage.disableVisibilityChange = true;
         
-
         fullCells = game.add.group();
         fullCells.enableBody = true;
         fullCells.createMultiple(3, "fullCell");
         
+        lives = game.add.group();
+        lives.enableBody = true;
+        lives.createMultiple(3, "life");
+        
         timerFull = game.time.events.loop(45000,this.newFullCell,this);
-        
-      
-        
+        timerLives = game.time.events.loop(60000,this.newLife,this);
+
         callback = {
 
             onSuccess:function () {
@@ -87,8 +89,10 @@ var statusMain = {
     },
     update: function () {
         
+        //Movement of the background
         bg.tilePosition.x -= 1;
         
+        //Movement of the spaceship
         if (cursorKeys.right.isDown) {
             player.angle += 4;
         } else if (cursorKeys.left.isDown) {
@@ -96,41 +100,67 @@ var statusMain = {
         }
         if (cursorKeys.up.isDown) {
             player.animations.play("acceleration");
+            
             player.body.velocity.x +=  Math.cos((player.angle - 90)*Math.PI/180) ;
-            //console.log("Velocidad en x: " + player.body.velocity.x  );
+            
             player.body.velocity.y +=  Math.sin((player.angle - 90)*Math.PI/180);
-            //console.log("Velocidad en y: " + player.body.velocity.y  );
+            
         } else {
             player.animations.stop("acceleration");
-            //player.body.acceleration = 0;
             player.frame = 0;
         }
+        
+        //collision of the cells with the spaceship
         game.physics.arcade.overlap(player, fullCells, this.takeFullCell, null,  this);
-//        var playerList = GameModule.getPlayerList();
-//        for (var _x = 0; _x < playerList.length; _x++) {
-//            
-//        }
+        //collision of the life with the spaceship
+        game.physics.arcade.overlap(player, lives, this.takeLife, null,  this);
+              
+        //Multiplayer position update
         if (connected === true) { ClientModule.sendUpdatePlayer(player.x, player.y, player.angle);}
     },
     newFullCell: function(){
-        console.log("que paso mi perro" );  
+          
         var fullCell = fullCells.getFirstDead();
         
-        fullCell.anchor.setTo(0.5);
-        fullCell.scale.setTo(0.7);
+        if(fullCell !== null){
+            fullCell.anchor.setTo(0.5);
+            fullCell.scale.setTo(0.7);
+
+            fullCell.reset(Math.floor(Math.random() * 801),Math.floor(Math.random() * 601));
+
+            game.physics.arcade.enable(fullCell);
+            fullCell.body.collideWorldBounds = true;
+            
+            //direction of the fullCell
+            if(Math.floor(Math.random() * 2) === 1){    fullCell.body.velocity.x = Math.floor(Math.random() * 30);
+            }else{  fullCell.body.velocity.x = -Math.floor(Math.random() * 30); }
+            
+            if(Math.floor(Math.random() * 2) === 1){    fullCell.body.velocity.y = Math.floor(Math.random() * 30);
+            }else{  fullCell.body.velocity.y = -Math.floor(Math.random() * 30); }
+
+            //bounce of the fullCell
+            fullCell.body.bounce.x = 1;
+            fullCell.body.bounce.y = 1;
+        }
         
-        fullCell.reset(Math.floor(Math.random() * 801),Math.floor(Math.random() * 601));
-        
-        
-        game.physics.arcade.enable(fullCell);
-        fullCell.body.collideWorldBounds = true;
-        fullCell.body.velocity.x = Math.floor(Math.random() * 30);
-        fullCell.body.velocity.y = Math.floor(Math.random() * 30);
-        fullCell.body.bounce.x = 1;
-        fullCell.body.bounce.y = 1;
     },
     takeFullCell: function(p, cell){
         cell.kill();
+    },
+    newLife: function(){
+        var life = lives.getFirstDead();
+        
+        if(life !== null){
+            life.anchor.setTo(0.5);
+            life.scale.setTo(0.7);
+
+            life.reset(Math.floor(Math.random() * 801),Math.floor(Math.random() * 601));
+
+            game.physics.arcade.enable(life);
+        }
+    },
+    takeLife: function(p, life){
+        life.kill();
     }
 };
 
