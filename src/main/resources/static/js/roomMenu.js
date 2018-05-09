0e530444518ea59271c6c64b626c85d54d1fea1c
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2018-05-08 21:41:42 -0500

    Agregado Menu de Salas, y cambios minimos 	- Menu de salas roomMenu.js 	- Cambios minimos en initialMenu

--- src/main/resources/static/js/roomMenu.js
+++ src/main/resources/static/js/roomMenu.js
@@ -1,34 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /* global Phaser, game, PhaserInput */
 
 var bgMI;
 
-var MenuInitial = {
+var RoomMenu = {
     preload: function () {
         game.load.image("backgroundInitialMenu", "assets/fondoMenu.png");
         game.load.image("boton","assets/btn.png");
     },
     create: function () {
         bgMI = game.add.tileSprite(0, 0, 800, 600, "backgroundInitialMenu");
-
-        game.add.plugin(PhaserInput.Plugin);
-
-        var userName = game.add.inputField(game.width / 2 - 84, game.height / 2 - 4, {
-            font: "18px Arial",
-            fill: "#212121",
-            fontWeight: "bold",
-            width: 150,
-            padding: 8,
-            borderWidth: 1,
-            borderColor: "#000",
-            borderRadius: 6,
-            placeHolder: "Nombre"
-        });
-
 
         var txtTitulo = game.add.text(game.width / 2, game.height / 2 - 120, "ASTEROIDS COOP", {
             font: "bold 60px sans-serif",
@@ -40,18 +20,31 @@ var MenuInitial = {
         grd.addColorStop(0, '#8ED6FF');
         grd.addColorStop(1, '#004CB3');
         txtTitulo.fill = grd;
-
-        var txtInicial = game.add.text(game.width / 2, game.height / 2 - 30, "Nombre de usuario", {
+        
+        var txtInicial = game.add.text(game.width / 2, game.height / 2 - 30, "Unirse a Sala", {
             font: "bold 24px sans-serif",
             fill: "white",
             align: "center"
         });
         txtInicial.anchor.setTo(0.5);
         
+        var roomId = game.add.inputField(game.width / 2 - 84, game.height / 2 - 4, {
+            font: "18px Arial",
+            fill: "#212121",
+            fontWeight: "bold",
+            width: 150,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: "#000",
+            borderRadius: 6,
+            placeHolder: "# de Sala",
+            type: PhaserInput.InputType.number
+        });
+        
         var boton = this.add.button(game.width / 2, game.height / 2 + 100, "boton", this.startGame, this);
         boton.anchor.setTo(0.5);
         boton.scale.setTo(0.5);
-
+        
         var txtDevelop = game.add.text(game.width / 2, game.height -30, "     Desarrollado  por  Juan Felipe Ortiz   y   Daniel Ospina       ", {
             font: "bold 12px sans-serif",
             fill: "#def4f4",
@@ -60,9 +53,9 @@ var MenuInitial = {
         txtDevelop.anchor.setTo(0.5);
     },
     update: function () {
-        bgMI.tilePosition.x -= 0.2;
+        bgMI.tilePosition.x += 0.2;
     },
     startGame: function () {
-        this.state.start("RoomMenu");
+        this.state.start("Game");
     }
 };
--- src/main/resources/static/js/statusController.js
+++ src/main/resources/static/js/statusController.js
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/* global Phaser, statusMain, MenuInitial */
+/* global Phaser, statusMain, MenuInitial, RoomMenu */
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, "canvasGame");
 
 game.state.add("Menu", MenuInitial);
+game.state.add("RoomMenu", RoomMenu);
 game.state.add("Game", statusMain);
 //game.state.add("GameOver", GameOver);
 
